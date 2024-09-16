46370c5dd5efbcb38ef150642dc16866877e2e99
Aneena Ginson <aginson7135@stu.d214.org>
2024-09-16 10:25:05 -0500

    @binding

---
+++ HeaderView.swift
@@ -0,0 +1,16 @@
+\//
+//  HeaderView.swift
+//  GitHubPractice3
+//
+//  Created by Aneena M. Ginson on 9/12/24.
+//
+
+//import Foundation
+import SwiftUI
+
+struct HeaderView: View {
+    
+    var body: some View {
+        @Binding tasks: [String] = []
+    }
+}
