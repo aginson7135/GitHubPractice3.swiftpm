b6e563085abe5a8b80bd3e334015915a3b73ab31
Aneena Ginson <aginson7135@stu.d214.org>
2024-09-16 10:34:45 -0500

    binding

--- HeaderView.swift
+++ HeaderView.swift
@@ -5,12 +5,17 @@
 //  Created by Aneena M. Ginson on 9/12/24.
 //
 
-//import Foundation
+import Foundation
 import SwiftUI
 
 struct HeaderView: View {
-    
+    @Binding var tasks: [String] = []
+    @State var = ("entered string")
     var body: some View {
-        @Binding tasks: [String] = []
-    }
-}
+//        HStack{
+//          TextField(text: <#T##Binding<String>#>, label: <#T##() -> Label#>)
+        }
+        }
+    
+
+
