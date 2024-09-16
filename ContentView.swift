dd35d7a532c23c1827871b8a490afd1e0574560d
Evan <egross6142@stu.d214.org>
2024-09-16 10:41:48 -0500

    pray

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var Array = ["Mow the Lawn", "Feed The Dog", "Grocery Shop"]
+    @State var Array:task = ["Mow the Lawn", "Feed The Dog", "Grocery Shop"]
     var body: some View {
         VStack {
             List(Array, id: \.self){
                 Array in
-                Text(Array)
+                Text(Array.nameOfTask)
             }
             Image(systemName: "globe")
                 .imageScale(.large)
--- Task.swift
+++ Task.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+struct task: Hashable{
+    var nameOfTask:String
+    
+}
